633687d806d058c8ff09b5b51acf94c76213e28d
Alex Hill <hill.alex.james@gmail.com>
2019-08-19 19:23:25 -0500

    initial

---
+++ WordCheater/WordCheater/LetterTree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordCheater
+{
+    class LetterTree
+    {
+
+        private List<letterNode> topLevelNodes;
+
+        public LetterTree()
+        {
+            topLevelNodes = new List<letterNode>();
+        }
+
+        public void addWord(string word)
+        {
+            if (!string.IsNullOrWhiteSpace(word))
+            {
+                letterNode firstNode = null;
+                string firstLetter = word[0].ToString();
+
+                foreach(var topNode in topLevelNodes)
+                {
+                    if (topNode.letter == firstLetter)
+                    {
+                        firstNode = topNode;
+                        break;
+                    }
+                }
+
+                if (firstNode == null)
+                {
+                    firstNode = new letterNode(firstLetter);
+                }
+
+                if(word.Length > 1)
+                {
+                    firstNode.addNodes(word.Substring(1));
+                }
+            }
+        }
+
+    }
+}
---
+++ WordCheater/WordCheater/letterNode.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordCheater
+{
+    class letterNode
+    {
+        private string _letter;
+        public List<letterNode> nodes;
+
+        public string letter
+        {
+            get
+            {
+                return _letter;
+            }
+            set
+            {
+                _letter = value;
+            }
+        }
+
+        private void buildNewNode(string newLetter)
+        {
+            _letter = newLetter;
+            nodes = new List<letterNode>();
+        }
+
+        public letterNode()
+        {
+            buildNewNode(null);
+        }
+
+        public letterNode(string letter)
+        {
+            buildNewNode(letter);
+        }
+
+        public letterNode contains(string nextLetter)
+        {
+            foreach(var node in nodes)
+            {
+                if(node.letter == nextLetter)
+                {
+                    return node;
+                }
+            }
+            return null; 
+        }
+
+        public void addNodes(string nextLetters)
+        {
+            var nextNode =  this.contains(nextLetters[0].ToString());
+
+            if(nextNode == null)
+            {
+                nextNode = new letterNode();
+            }
+            nextNode.letter = nextLetters[0].ToString();
+
+            nodes.Add(nextNode);
+
+            if(nextLetters.Length > 1)
+            {
+                nextNode.addNodes(nextLetters.Substring(1));
+            }
+        }
+    }
+}
